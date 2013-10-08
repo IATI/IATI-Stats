3b2ccf6d604a1aa6e57c504434e9579899496519
Ben Webb <bjwebb67@googlemail.com>
2013-10-08 09:05:42 +0100

    Integrate gitdate

--- git.sh
+++ git.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
-rm -r out
+# Clear gitout
 cd gitout || exit $?
 git rm -r *
 rm -rf *
 cd .. || exit $?
+
+# Clear other output directories
 rm -r aggregated* inverted* out*
+
 cd data || exit $?
+# Checkout master, and make sure it is clean and up to date
 git checkout master
 git reset --hard
 git clean -df
 git pull --ff-only
+
+# Create gitdate file
+echo '{' > gitdate.json
+git log --format="format:%H|%ai" | awk -F '|' '{ print "\""$1"\": \""$2"\"," } ' >> gitdate.json
+# Ensure the last line doesn't have a trailing comma
+sed -i '$d' gitdate.json
+git log --format="format:%H|%ai" | tail -n1 | awk -F '|' '{ print "\""$1"\": \""$2"\"" } ' >> gitdate.json
+echo '}' >> gitdate.json
+# Perform this dance because piping to ../ behaves differently with symlinks
+cd .. || exit $?
+mv data/gitdate.json .
+cd data || exit $?
+
+
 for commit in `git log --format=format:%H`; do
     git checkout $commit
     git clean -df
@@ -28,10 +46,8 @@ done
 
 cd ..
 python2 gitaggregate.py > gitout/gitaggregate.json
-./gitdate.sh
+mv gitdate.json gitout
 cp -r html gitout
-rm gitout/html/gitout
-ln -s .. gitout/html/gitout
 
 cd gitout
 git add .

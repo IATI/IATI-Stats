42040d8f0fc94e4333179f7155ded55d02318113
Ben Webb <bjwebb67@googlemail.com>
2014-03-20 12:07:28 +0000

    Nested hash directories, fixes #32

--- statsrunner/hashcopy.py
+++ statsrunner/hashcopy.py
@@ -8,7 +8,8 @@ gitout_dir = os.path.join(os.getcwd(),'gitout')
 for publisher in os.listdir(base):
     for dataset in os.listdir(os.path.join(base, publisher)):
         md5hash = hashlib.md5(open(os.path.join(data_dir, publisher, dataset)).read()).hexdigest()
-        if not os.path.exists(os.path.join('gitout','hash',md5hash)):
-            shutil.move(os.path.join(base, publisher, dataset), os.path.join(gitout_dir,'hash',md5hash))
-            os.symlink(os.path.join(gitout_dir,'hash',md5hash), os.path.join(base, publisher, dataset))
+        hashdir = os.path.join(md5hash[0], md5hash[1], md5hash[2], md5hash[3], md5hash)
+        if not os.path.exists(os.path.join('gitout','hash',hashdir)):
+            shutil.move(os.path.join(base, publisher, dataset), os.path.join(gitout_dir,'hash',hashdir))
+            os.symlink(os.path.join(gitout_dir,'hash',hashdir), os.path.join(base, publisher, dataset))
 

1a0e3ccaac0f3a9c9c09643cde3bbae848d3d727
Ben Webb <bjwebb67@googlemail.com>
2014-10-23 10:28:40 +0100

    [IATI/IATI-Dashboard#218] Remove forwardlooking fallback
    (to planned disbursements)

--- stats/dashboard.py
+++ stats/dashboard.py
@@ -337,16 +337,10 @@ class ActivityStats(CommonSharedElements):
     def forwardlooking_currency_year(self):
         out = defaultdict(lambda: defaultdict(Decimal))
         budgets = self.element.findall('budget')
-        if len(budgets):
-            for budget in budgets:
-                value = budget.find('value')
-                currency = value.attrib.get('currency') or self.element.attrib.get('default-currency')
-                out[budget_year(budget)][currency] += Decimal(value.text)
-        else:
-            for planned_disbursement in self.element.findall('planned-disbursement'):
-                value = planned_disbursement.find('value')
-                currency = value.attrib.get('currency') or self.element.attrib.get('default-currency')
-                out[budget_year(planned_disbursement)][currency] += Decimal(value.text)
+        for budget in budgets:
+            value = budget.find('value')
+            currency = value.attrib.get('currency') or self.element.attrib.get('default-currency')
+            out[budget_year(budget)][currency] += Decimal(value.text)
         return out
 
     def _transaction_type_code(self, transaction):

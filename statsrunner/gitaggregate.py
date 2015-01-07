c9b1bd155ed023e7933f375162ff887b1e3b1d26
Ben Webb <bjwebb67@googlemail.com>
2015-01-07 14:50:31 +0000

    Fix gitaggregate.py so that new json files will be created
    (ensures that tests pass)

--- statsrunner/gitaggregate.py
+++ statsrunner/gitaggregate.py
@@ -15,27 +15,31 @@ try:
 except OSError:
     pass
 
-for fname in os.listdir(git_out_dir):
-    if fname.endswith('.json'):
+git_out_files = os.listdir(git_out_dir)
+
+for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
+    print commit
+    for fname in os.listdir(os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated')):
+        if not fname.endswith('.json'):
+            continue
+        k = fname[:-5]
+        if k in ['codelist_values','iati_identifiers','duplicate_identifiers','publisher_duplicate_identifiers', 'participating_orgs_text','transaction_dates']:
+           continue
         print fname
-        with open(os.path.join(git_out_dir, fname)) as fp:
-            k = fname[:-5]
-            if k in ['codelist_values','iati_identifiers','duplicate_identifiers','publisher_duplicate_identifiers', 'participating_orgs_text','transaction_dates']:
-               continue
-            v = json.load(fp, parse_float=decimal.Decimal)
-            for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
-                if not commit in v:
-                    commit_json_fname = os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated', fname)
-                    try:
-                        with open(commit_json_fname) as fp2:
-                            v2 = json.load(fp2, parse_float=decimal.Decimal)
-                            if dated:
-                                if commit in gitdates:
-                                    v[gitdates[commit]] = v2
-                            else:
-                                v[commit] = v2
-                    except IOError:
-                        print 'Could not open {}'.format(commit_json_fname)
+        commit_json_fname = os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated', fname)
+        if fname in git_out_files:
+            with open(os.path.join(git_out_dir, fname)) as fp:
+                v = json.load(fp, parse_float=decimal.Decimal)
+        else:
+            v = {}
+        if not commit in v:
+            with open(commit_json_fname) as fp2:
+                v2 = json.load(fp2, parse_float=decimal.Decimal)
+                if dated:
+                    if commit in gitdates:
+                        v[gitdates[commit]] = v2
+                else:
+                    v[commit] = v2
 
             with open(os.path.join(git_out_dir, k+'.json.new'), 'w') as fp:
                 json.dump(v, fp, sort_keys=True, indent=2, default=decimal_default)

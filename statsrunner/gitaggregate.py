b5ccf4c201fd252feb01a25ce5f56dbf42e404de
Ben Webb <bjwebb67@googlemail.com>
2015-01-07 14:29:33 +0000

    Don't load all gitaggregate json into memory at once

--- statsrunner/gitaggregate.py
+++ statsrunner/gitaggregate.py
@@ -15,29 +15,28 @@ try:
 except OSError:
     pass
 
-total = defaultdict(dict)
 for fname in os.listdir(git_out_dir):
     if fname.endswith('.json'):
         print fname
         with open(os.path.join(git_out_dir, fname)) as fp:
-            total[fname[:-5]] = json.load(fp, parse_float=decimal.Decimal)
+            k = fname[:-5]
+            if k in ['codelist_values','iati_identifiers','duplicate_identifiers','publisher_duplicate_identifiers', 'participating_orgs_text','transaction_dates']:
+               continue
+            v = json.load(fp, parse_float=decimal.Decimal)
+            for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
+                if not commit in v:
+                    commit_json_fname = os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated', fname)
+                    try:
+                        with open(commit_json_fname) as fp2:
+                            v2 = json.load(fp2, parse_float=decimal.Decimal)
+                            if dated:
+                                if commit in gitdates:
+                                    v[gitdates[commit]] = v2
+                            else:
+                                v[commit] = v2
+                    except IOError:
+                        print 'Could not open {}'.format(commit_json_fname)
 
-for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
-    for fname in os.listdir(os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated')):
-        if fname.endswith('.json'):
-            with open(os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated', fname)) as fp:
-                k = fname[:-5]
-                if k in ['codelist_values','iati_identifiers','duplicate_identifiers','publisher_duplicate_identifiers', 'participating_orgs_text','transaction_dates']:
-                   continue
-                if not commit in total[k]:
-                    v = json.load(fp, parse_float=decimal.Decimal)
-                    if dated:
-                        if commit in gitdates:
-                            total[k][gitdates[commit]] = v
-                    else:
-                        total[k][commit] = v
-
-for k,v in total.items():
-    with open(os.path.join(git_out_dir, k+'.json.new'), 'w') as fp:
-        json.dump(v, fp, sort_keys=True, indent=2, default=decimal_default)
-    os.rename(os.path.join(git_out_dir, k+'.json.new'), os.path.join(git_out_dir, k+'.json'))
+            with open(os.path.join(git_out_dir, k+'.json.new'), 'w') as fp:
+                json.dump(v, fp, sort_keys=True, indent=2, default=decimal_default)
+            os.rename(os.path.join(git_out_dir, k+'.json.new'), os.path.join(git_out_dir, k+'.json'))

fe6dc188f5df82c86ff1d2b78bbd2a6765599fd7
dalepotter <dalepotter@gmail.com>
2015-07-24 15:10:35 +0100

    Fix logic for current activites to include an actual end date  between one year ago and now

--- stats/dashboard.py
+++ stats/dashboard.py
@@ -599,20 +599,16 @@ class ActivityStats(CommonSharedElements):
         if not(activity_planned_end_dates) and activity_status_code:
             if activity_status_code[0] == '2' or activity_status_code[0] == '4':
                 return True
-        
+
         # If the actual end date is within the last year, then this is a current activity
         for actual_end_date in activity_actual_end_dates: 
-            if actual_end_date>=add_years(self.today, -1):
+            if (actual_end_date>=add_years(self.today, -1)) and (actual_end_date <= self.today):
                 return True
-            else:
-                return False
         
         # If the planned end date is greater than today, then this is a current activity
         for planned_end_date in activity_planned_end_dates: 
             if planned_end_date>=self.today:
                 return True
-            else:
-                return False
         
         # If got this far and not met one of the conditions to qualify as a currnet activity, return false
         return False

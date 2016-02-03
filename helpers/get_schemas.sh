2740540e83c88dc06090d88ac711decf7d1265ae
dalepotter <dalepotter@gmail.com>
2016-02-03 14:58:17 +0000

    [IATI/IATI-Dashboard#379] Update Stats code for 2.02

--- helpers/get_schemas.sh
+++ helpers/get_schemas.sh
@@ -1,6 +1,6 @@
 mkdir schemas
 cd schemas
-for v in 1.01 1.02 1.03 1.04 1.05 2.01; do
+for v in 1.01 1.02 1.03 1.04 1.05 2.01 2.02; do
     git clone https://github.com/IATI/IATI-Schemas.git $v
     cd $v
     git checkout version-$v

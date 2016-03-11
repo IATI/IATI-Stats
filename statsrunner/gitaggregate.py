294c2ef5e21c246aeb8adc050272e7fa1573c5dd
dalepotter <dalepotter@gmail.com>
2016-03-11 16:13:50 +0000

    Improve documentation and logging output

--- statsrunner/gitaggregate.py
+++ statsrunner/gitaggregate.py
@@ -1,37 +1,58 @@
-import json, os, sys
 from collections import defaultdict
-import decimal
 from common import decimal_default
+import decimal
+import json
+import os 
+import sys
 
+# Set value for the gitout directory
 GITOUT_DIR = os.environ.get('GITOUT_DIR') or 'gitout'
 
+# Set bool if the 'dated' argument has been used in calling this script
 dated = len(sys.argv) > 1 and sys.argv[1] == 'dated'
+
 git_out_dir = os.path.join(GITOUT_DIR, 'gitaggregate-dated' if dated else 'gitaggregate')
+
+# Load in the reference of commits to dates 
 if dated:
     gitdates = json.load(open('gitdate.json'))
 
+# Make the gitout directory
 try:
     os.makedirs(git_out_dir)
 except OSError:
     pass
 
+# Get a list containing the names of the entries in the directory
 git_out_files = os.listdir(git_out_dir)
 
+# Loop over each commit in gitout/commits
 for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
-    print commit
+    print 'Aggregating for commit: {}'.format(commit)
+
     for fname in os.listdir(os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated')):
         if not fname.endswith('.json'):
             continue
-        k = fname[:-5]
+        
+        k = fname[:-5] # remove '.json' from the filename
+        # Ignore certain files
         if k in ['codelist_values','iati_identifiers','duplicate_identifiers','publisher_duplicate_identifiers', 'participating_orgs_text','transaction_dates']:
            continue
-        print fname
+
+        print 'Adding to {} for file: {}'.format('gitaggregate-dated' if dated else 'gitaggregate', fname)
+        
         commit_json_fname = os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated', fname)
+        
+        # Load the current file conents to memory, or set as an empty dictionary
         if fname in git_out_files:
+            # FIXME: This is a possible cause of a memory issue in future, as the size of the aggregate file 
+            #        increases each time there is a new commit
             with open(os.path.join(git_out_dir, fname)) as fp:
                 v = json.load(fp, parse_float=decimal.Decimal)
         else:
             v = {}
+        
+        # If the commit that we are looping over is not already in the data for this file, then add it to the output
         if not commit in v:
             with open(commit_json_fname) as fp2:
                 v2 = json.load(fp2, parse_float=decimal.Decimal)
@@ -41,6 +62,9 @@ for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
                 else:
                     v[commit] = v2
 
+            # Write output to a temporary file, then rename
             with open(os.path.join(git_out_dir, k+'.json.new'), 'w') as fp:
+                print 'Writing data to {}'.format(k)
                 json.dump(v, fp, sort_keys=True, indent=2, default=decimal_default)
+            print 'Renaming file {} to {}'.format(k+'.json.new', k+'.json')
             os.rename(os.path.join(git_out_dir, k+'.json.new'), os.path.join(git_out_dir, k+'.json'))

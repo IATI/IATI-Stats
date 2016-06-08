cc4d3684bc5c6bf83e469b4e804d52274818bbd8
dalepotter <dalepotter@gmail.com>
2016-06-08 11:48:09 +0100

    Add lists for black/whitelisted stats to gitaggregate scripts

--- statsrunner/gitaggregate-publisher.py
+++ statsrunner/gitaggregate-publisher.py
@@ -5,6 +5,28 @@ from common import decimal_default
 
 GITOUT_DIR = os.environ.get('GITOUT_DIR') or 'gitout'
 
+# Only aggregate certain json stats files at publisher level 
+# These should be small stats files that will not consume large amounts of 
+# memory/disk space if aggregated over time
+whitelisted_stats_files - [ 
+    'activities',
+    'activity_files',
+    'bottom_hierarchy',
+    'empty',
+    'invalidxml',
+    'file_size',
+    'nonstandardroots',
+    'organisation_files',
+    'publisher_unique_identifiers',
+    'toolarge',
+    'validation',
+    'versions',
+    'activities_with_future_transactions',
+    'latest_transaction_date',
+    'transaction_dates_hash',
+    'most_recent_transaction_date'
+    ]
+
 dated = len(sys.argv) > 1 and sys.argv[1] == 'dated'
 if dated:
     gitdates = json.load(open('gitdate.json'))
@@ -24,9 +46,7 @@ for commit in os.listdir(os.path.join(GITOUT_DIR, 'commits')):
                     with open(os.path.join(git_out_dir, fname)) as fp:
                         total[fname[:-5]] = json.load(fp, parse_float=decimal.Decimal)
 
-        for statname in [ 'activities', 'activity_files', 'bottom_hierarchy', 'empty', 'invalidxml', 'file_size', 'nonstandardroots', 'organisation_files', 'publisher_unique_identifiers', 'toolarge', 'validation', 'versions', # Whitelist small stats to avoid using too much diskspace
-            'activities_with_future_transactions',
-                'latest_transaction_date', 'transaction_dates_hash', 'most_recent_transaction_date' ]: # Only run for stats/transaction_dates.py
+        for statname in whitelisted_stats_files:
             path = os.path.join(GITOUT_DIR, 'commits', commit, 'aggregated-publisher', publisher, statname+'.json')
             if os.path.isfile(path):
                 with open(path) as fp:

8159dcd08c1e2c5d3e8b1bd080ca43e2ae15f655
Hayden <hayden.field@devinit.org>
2017-06-01 17:03:51 +0100

    Restrict where sector vocab 10 is accepted

--- stats/dashboard.py
+++ stats/dashboard.py
@@ -1212,7 +1212,7 @@ class ActivityStats(CommonSharedElements):
             'is_humanitarian': is_humanitarian,
             'is_humanitarian_by_attrib': is_humanitarian_by_attrib,
             'contains_humanitarian_scope': 1 if (self._version() in ['2.02']) and self.element.xpath('humanitarian-scope/@type') and self.element.xpath('humanitarian-scope/@code') else 0,
-            'uses_humanitarian_clusters_vocab': 1 if (self._major_version() in ['2']) and self.element.xpath('sector/@vocabulary="10"') else 0
+            'uses_humanitarian_clusters_vocab': 1 if (self._version() in ['2.02']) and self.element.xpath('sector/@vocabulary="10"') else 0
         }
 
     def _transaction_type_code(self, transaction):

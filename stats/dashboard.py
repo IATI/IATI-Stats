f1fc0240225d2c8fd201571e76bdde3f224de2a7
Hayden <hayden.field@devinit.org>
2017-06-01 14:16:07 +0100

    Detect 5 digit sectors at transaction level

--- stats/dashboard.py
+++ stats/dashboard.py
@@ -1189,9 +1189,10 @@ class ActivityStats(CommonSharedElements):
         is_humanitarian_by_attrib_transaction = 1 if set(self.element.xpath('transaction/@humanitarian')).intersection(['1', 'true']) else 0
         is_humanitarian_by_attrib = (self._version() in ['2.02']) and (is_humanitarian_by_attrib_activity or is_humanitarian_by_attrib_transaction)
 
-        is_humanitarian_by_sector_5_digit = 1 if set(self.element.xpath('sector[@vocabulary="{0}" or not(@vocabulary)]/@code'.format(self._dac_5_code()))).intersection(humanitarian_sectors_dac_5_digit) else 0
+        is_humanitarian_by_sector_5_digit_activity = 1 if set(self.element.xpath('sector[@vocabulary="{0}" or not(@vocabulary)]/@code'.format(self._dac_5_code()))).intersection(humanitarian_sectors_dac_5_digit) else 0
+        is_humanitarian_by_sector_5_digit_transaction = 1 if set(self.element.xpath('transaction/sector[@vocabulary="{0}" or not(@vocabulary)]/@code'.format(self._dac_5_code()))).intersection(humanitarian_sectors_dac_5_digit) else 0
         is_humanitarian_by_sector_3_digit = 1 if set(self.element.xpath('sector[@vocabulary="{0}"]/@code'.format(self._dac_3_code()))).intersection(humanitarian_sectors_dac_3_digit) else 0
-        is_humanitarian_by_sector = is_humanitarian_by_sector_5_digit or is_humanitarian_by_sector_3_digit
+        is_humanitarian_by_sector = is_humanitarian_by_sector_5_digit_activity or is_humanitarian_by_sector_5_digit_transaction or is_humanitarian_by_sector_3_digit
 
         return {
             'is_humanitarian': 1 if (is_humanitarian_by_attrib or is_humanitarian_by_sector) else 0,

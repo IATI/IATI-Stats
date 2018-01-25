9c050b04ffbd9e41033b5693f7bf89819f837d66
dalepotter <dalepotter@gmail.com>
2018-01-25 12:02:14 +0000

    Add support for IATI version 2.03
    Fixes #117

--- helpers/get_schemas.sh
+++ helpers/get_schemas.sh
@@ -2,7 +2,7 @@
 
 mkdir schemas
 cd schemas
-for v in 1.01 1.02 1.03 1.04 1.05 2.01 2.02; do
+for v in 1.01 1.02 1.03 1.04 1.05 2.01 2.02 2.03; do
     git clone https://github.com/IATI/IATI-Schemas.git $v
     cd $v
     git checkout version-$v

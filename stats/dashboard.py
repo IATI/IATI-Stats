bd984e4ac3c03dbda368604a38a75c4a4c97f078
Andy Lulham <a.lulham@gmail.com>
2018-06-13 13:23:51 +0100

    Refactor for clarity

--- stats/dashboard.py
+++ stats/dashboard.py
@@ -1228,8 +1228,15 @@ class ActivityStats(CommonSharedElements):
         return {
             'is_humanitarian': is_humanitarian,
             'is_humanitarian_by_attrib': is_humanitarian_by_attrib,
-            'contains_humanitarian_scope': 1 if (self._version() in ['2.02', '2.03']) and self.element.xpath('humanitarian-scope/@type') and self.element.xpath('humanitarian-scope/@code') else 0,
-            'uses_humanitarian_clusters_vocab': 1 if (self._version() in ['2.02', '2.03']) and self.element.xpath('sector/@vocabulary="10"') else 0
+            'contains_humanitarian_scope': 1 if (
+                self._version() in ['2.02', '2.03'] and
+                self.element.xpath('humanitarian-scope/@type') and
+                self.element.xpath('humanitarian-scope/@code')
+            ) else 0,
+            'uses_humanitarian_clusters_vocab': 1 if (
+                self._version() in ['2.02', '2.03'] and
+                self.element.xpath('sector/@vocabulary="10"')
+            ) else 0
         }
 
     def _transaction_type_code(self, transaction):

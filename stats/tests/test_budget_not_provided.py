061c4f7c3ec13e67da84ebf62ee2cab22a8c1f6a
Ocre42 <john.askew@devinit.org>
2018-12-13 10:13:19 +0000

    Adding budget-not-provided methods to dashboard.py

---
+++ stats/tests/test_budget_not_provided.py
@@ -0,0 +1,38 @@
+from lxml import etree
+
+from collections import defaultdict, OrderedDict
+from stats.common.decorators import *
+
+def test_budget_not_provided_works():
+    activity_stats = ActivityStats()
+    activity_stats.element = etree.fromstring('''
+            <iati-activity budget-not-provided="1">
+            </iati-activity>
+    ''')
+    assert activity_stats._budget_not_provided() == {'budget-not-provided': '1'}
+
+def test_budget_not_provided_fails():
+    activity_stats = ActivityStats()
+    activity_stats.element = etree.fromstring('''
+            <iati-activity>
+            </iati-activity>
+    ''')
+    assert activity_stats._budget_not_provided() == None
+
+
+class CommonSharedElements(object):
+    blank = False
+
+
+class ActivityStats(CommonSharedElements):
+    """ Stats calculated on a single iati-activity. """
+    element = None
+    blank = False
+    strict = False # (Setting this to true will ignore values that don't follow the schema)
+    context = ''
+    comprehensiveness_current_activity_status = None
+    now = datetime.datetime.now()
+
+    def _budget_not_provided(self):
+        if self.element.attrib.get('budget-not-provided'):
+            return {'budget-not-provided': self.element.attrib.get('budget-not-provided')}

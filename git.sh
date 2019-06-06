97e278cf436fdfbe289e82467661f8b07208d8d5
Ocre42 <john.askew@devinit.org>
2019-06-06 15:51:43 +0100

    Adding dated to aggregated-publisher and secondary reporting for stats

--- git.sh
+++ git.sh
@@ -11,7 +11,9 @@ fi
 # Make the all the gitout directories
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Making gitout directories"
 mkdir -p $GITOUT_DIR/logs
+mkdir -p $GITOUT_DIR/commits
 mkdir -p $GITOUT_DIR/gitaggregate
+mkdir -p $GITOUT_DIR/gitaggregate-dated
 
 
 cd helpers
@@ -35,19 +37,107 @@ cp helpers/ckan.json $GITOUT_DIR
 echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Clearing output directory"
 rm -r out
 
-# Loop over commits and run stats code
-# Run the stats commands and save output to log files
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (loop)"
-python calculate_stats.py loop > $GITOUT_DIR/logs/loop.log || exit 1
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (aggregate)"
-python calculate_stats.py aggregate > $GITOUT_DIR/logs/aggregate.log || exit 1
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (invert)"
-python calculate_stats.py invert > $GITOUT_DIR/logs/invert.log || exit 1
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Moving json files to output folder"
-mv -v out/* $GITOUT_DIR
+cd data || exit $?
+# Checkout automatic, and make sure it is clean and up to date
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Performing git operations on the data snapshot"
+git checkout automatic
+git reset --hard
+git clean -df
+git pull --ff-only
 
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running gitaggregate.py"
-python statsrunner/gitaggregate.py
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running gitaggregate-publisher.py"
-python statsrunner/gitaggregate-publisher.py
-echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Stats calculation complete"
+
+# Create a gitdate file in JSON format. This contains the git hash and date for each data commit
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Creating gitdate file"
+echo '{' > gitdate.json
+git log --format="format:%H|%ai" | awk -F '|' '{ print "\""$1"\": \""$2"\"," } ' >> gitdate.json
+# Ensure the last line doesn't have a trailing comma
+sed -i '$d' gitdate.json
+git log --format="format:%H|%ai" | tail -n1 | awk -F '|' '{ print "\""$1"\": \""$2"\"" } ' >> gitdate.json
+echo '}' >> gitdate.json
+# Perform this dance because piping to ../ behaves differently with symlinks
+cd .. || exit $?
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Move and copy gitdate file"
+mv data/gitdate.json .
+cp gitdate.json $GITOUT_DIR
+
+# Store current and all commit hashes as variables
+cd data || exit $?
+# Get the latest commit hash
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Storing git info as bash variables"
+current_hash=`git rev-parse HEAD`
+# Get all commit hashes
+commits=`git log --format=format:%H`
+cd .. || exit $?
+
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Looping over commits"
+for commit in $commits; do
+    if grep -q $commit $COMMIT_SKIP_FILE; then
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Skipping $commit (due to grep conditional)"
+    elif [ $GITOUT_SKIP_INCOMMITSDIR ] && [ -d $GITOUT_DIR/commits/$commit ]; then
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Skipping $commit (due to git skip conditionals)"
+    else
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running stats code for commit: $commit"
+
+        # Get the data to the specified commit
+        cd data || exit $?
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Checking out commit $commit"
+        git checkout $commit
+        git clean -df
+
+        commit_date=`git log --format="format:%ai" | head -n 1`
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Set commit date as $commit_date"
+        cd .. || exit $?
+
+        # Disable this because it doesn't work for date dependent stuff.........
+        #mkdir -p $GITOUT_DIR/hash
+        #python statsrunner/hashlink.py || exit 1
+        # (and also this on the next line: --new)
+
+        # Run the stats commands and save output to log files
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (loop) for commit $commit"
+        python calculate_stats.py $@ --today "$commit_date" loop > $GITOUT_DIR/logs/${commit}_loop.log || exit 1
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (aggregate) for commit $commit"
+        python calculate_stats.py $@ --today "$commit_date" aggregate > $GITOUT_DIR/logs/${commit}_aggregate.log || exit 1
+        if [ $commit = $current_hash ]; then
+		echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Calculating stats (invert) for commit $commit"
+    python calculate_stats.py $@ --today "$commit_date" invert > $GITOUT_DIR/logs/${commit}_invert.log
+        fi
+        #python statsrunner/hashcopy.py || exit 1
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Removing output for commit dir: $commit"
+        rm -r $GITOUT_DIR/commits/$commit
+        mv out $GITOUT_DIR/commits/$commit || exit $?
+
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running gitaggregate.py for commit: $commit"
+        python statsrunner/gitaggregate.py
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running gitaggregate-publisher.py for commit: $commit"
+        python statsrunner/gitaggregate-publisher.py
+        echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Running gitaggregate-publisher.py dated for commit: $commit"
+        python statsrunner/gitaggregate-publisher.py dated
+        # If the commit is the latest commit then, move the resulting stats to the 'current' directory
+        if [ ! $commit = $current_hash ]; then
+            echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Removing commit dir (based on latest commit logic) for commit: $commit"
+            rm -r $GITOUT_DIR/commits/$commit
+        else
+            cd $GITOUT_DIR || exit $?
+            echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Removing current dir for commit: $commit"
+            rm -r current
+            # Since we're not currently creating symlinks, we can just do a plain move here
+            mv commits/$current_hash current
+            echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Creating current.tar.gz for commit: $commit"
+            tar -czf current.tar.gz current
+            cd .. || exit $?
+        fi
+        if [ "$ALL_COMMITS" = "" ]; then
+            echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Breaking out of commit logic for commit: $commit"
+            break
+        fi
+    fi
+done
+
+cd $GITOUT_DIR || exit $?
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Creating compressed file: gitaggregate"
+tar -czf gitaggregate.tar.gz gitaggregate
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Creating compressed file: gitaggregate-publisher"
+tar -czf gitaggregate-publisher.tar.gz gitaggregate-publisher
+echo "LOG: `date '+%Y-%m-%d %H:%M:%S'` - Creating compressed file: gitaggregate-publisher-dated"
+tar -czf gitaggregate-publisher-dated.tar.gz gitaggregate-publisher-dated
--- stats/dashboard.py
+++ stats/dashboard.py
@@ -559,6 +559,24 @@ class ActivityStats(CommonSharedElements):
                 out[self._transaction_year(transaction)][get_currency(self, transaction)] += transaction_value
         return out
 
+    def _is_secondary_reported(self):
+        """Tests if this activity has been secondary reported. Test based on if the
+           secondary-reporter flag is set.
+        Input -- None
+        Output:
+          True -- Secondary-reporter flag set
+          False -- Secondary-reporter flag not set, or evaulates to False
+        """
+        return bool(filter(lambda x: int(x) if str(x).isdigit() else 0,
+                    self.element.xpath('reporting-org/@secondary-reporter')))
+
+    @returns_dict
+    def activities_secondary_reported(self):
+        if self._is_secondary_reported():
+            return {self.element.find('iati-identifier').text: 0}
+        else:
+            return {}
+
     @returns_numberdictdict
     def forwardlooking_currency_year(self):
         # Note this is not currently displayed on the dashboard

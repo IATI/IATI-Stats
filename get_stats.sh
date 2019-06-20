c1e1d042706161af5a325d05e8ca620bb1d0bec6
Ocre42 <john.askew@devinit.org>
2019-06-20 14:22:38 +0100

    Create setup script and update README

--- get_stats.sh
+++ get_stats.sh
@@ -1,34 +1,37 @@
 set -e
 # The above line ensures this scripts stops if any individual command errors
 
-mkdir gitout
+if [ ! -d outputs ]; then
+	mkdir outputs
+fi
 for f in ckan gitdate; do
-    curl --compressed "http://dashboard.iatistandard.org/stats/${f}.json" > gitout/${f}.json
+    curl --compressed "http://dashboard.iatistandard.org/stats/${f}.json" > outputs/${f}.json
 done
 
-mkdir stats-blacklist
+if [ ! -d stats-blacklist ]; then
+	mkdir stats-blacklist
+fi
 cd stats-blacklist
 wget "http://dashboard.iatistandard.org/stats-blacklist/current.tar.gz" -O current.tar.gz 
 wget "http://dashboard.iatistandard.org/stats-blacklist/gitaggregate.tar.gz" -O gitaggregate.tar.gz
 wget "http://dashboard.iatistandard.org/stats-blacklist/gitaggregate-publisher.tar.gz" -O gitaggregate-publisher.tar.gz
 wget "http://dashboard.iatistandard.org/stats-blacklist/gitaggregate-dated.tar.gz" -O gitaggregate-dated.tar.gz
 wget "http://dashboard.iatistandard.org/stats-blacklist/gitaggregate-publisher-dated.tar.gz" -O gitaggregate-publisher-dated.tar.gz
-tar -xvf current.tar.gz
-tar -xvf gitaggregate.tar.gz 
-tar -xvf gitaggregate.tar.gz
-tar -xvf gitaggregate-dated.tar.gz 
-tar -xvf gitaggregate-publisher-dated.tar.gz
+tar -xvzf current.tar.gz
+tar -xvzf gitaggregate.tar.gz 
+tar -xvzf gitaggregate-publisher.tar.gz
+tar -xvzf gitaggregate-dated.tar.gz 
+tar -xvzf gitaggregate-publisher-dated.tar.gz
 cd ..
 
-cd gitout
+cd outputs
 wget "http://dashboard.iatistandard.org/stats/current.tar.gz" -O current.tar.gz
 wget "http://dashboard.iatistandard.org/stats/gitaggregate.tar.gz" -O gitaggregate.tar.gz
 wget "http://dashboard.iatistandard.org/stats/gitaggregate-publisher.tar.gz" -O gitaggregate-publisher.tar.gz
 wget "http://dashboard.iatistandard.org/stats/gitaggregate-dated.tar.gz" -O gitaggregate-dated.tar.gz
 wget "http://dashboard.iatistandard.org/stats/gitaggregate-publisher-dated.tar.gz" -O gitaggregate-publisher-dated.tar.gz
-tar -xvf current.tar.gz
-tar -xvf gitaggregate.tar.gz 
-tar -xvf gitaggregate.tar.gz
-tar -xvf gitaggregate-dated.tar.gz 
-tar -xvf gitaggregate-publisher-dated.tar.gz
-
+tar -xvzf current.tar.gz
+tar -xvzf gitaggregate.tar.gz 
+tar -xvzf gitaggregate-publisher.tar.gz
+tar -xvzf gitaggregate-dated.tar.gz 
+tar -xvzf gitaggregate-publisher-dated.tar.gz

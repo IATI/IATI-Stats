78259105bea1ea62f8932182248f31908db2c3d0
James B <james.baster@opendataservices.coop>
2024-03-14 08:42:07 +0000

    wget should retry refused connections
    Also, set retry variables specifically rather than rely on defaults.
    It's clearer that we want retries and we may tweak them later.

--- helpers/get_codelists.sh
+++ helpers/get_codelists.sh
@@ -3,12 +3,12 @@
 for x in 105 203; do
     i=$(echo $x | head -c 1)
     mkdir -p codelists/$i
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Version.json" -O codelists/$i/Version.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/ActivityStatus.json" -O codelists/$i/ActivityStatus.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Currency.json" -O codelists/$i/Currency.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Sector.json" -O codelists/$i/Sector.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/SectorCategory.json" -O codelists/$i/SectorCategory.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/DocumentCategory.json" -O codelists/$i/DocumentCategory.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/AidType.json" -O codelists/$i/AidType.json
-    wget "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/BudgetNotProvided.json" -O codelists/$i/BudgetNotProvided.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Version.json" -O codelists/$i/Version.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/ActivityStatus.json" -O codelists/$i/ActivityStatus.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Currency.json" -O codelists/$i/Currency.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/Sector.json" -O codelists/$i/Sector.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/SectorCategory.json" -O codelists/$i/SectorCategory.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/DocumentCategory.json" -O codelists/$i/DocumentCategory.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/AidType.json" -O codelists/$i/AidType.json
+    wget --tries=20 --waitretry=10 --retry-connrefused "http://iatistandard.org/$x/codelists/downloads/clv2/json/en/BudgetNotProvided.json" -O codelists/$i/BudgetNotProvided.json
 done

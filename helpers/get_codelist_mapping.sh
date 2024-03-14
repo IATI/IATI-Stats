c6d823b75c8b532c63b1b18922c23c1d763148e4
James B <james.baster@opendataservices.coop>
2024-03-14 08:42:07 +0000

    wget should retry refused connections
    Also, set retry variables specifically rather than rely on defaults.
    It's clearer that we want retries and we may tweak them later.

--- helpers/get_codelist_mapping.sh
+++ helpers/get_codelist_mapping.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-wget https://raw.github.com/IATI/IATI-Codelists/version-1.05/mapping.xml -O mapping-1.xml
-wget https://raw.github.com/IATI/IATI-Codelists/version-2.03/mapping.xml -O mapping-2.xml
+wget --tries=20 --waitretry=10 --retry-connrefused https://raw.github.com/IATI/IATI-Codelists/version-1.05/mapping.xml -O mapping-1.xml
+wget --tries=20 --waitretry=10 --retry-connrefused https://raw.github.com/IATI/IATI-Codelists/version-2.03/mapping.xml -O mapping-2.xml
